5a718d16f03f4d49ac5d1459f7b0a67802e66301
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-02 22:41:24 +0530

    Update start.py

--- player/start.py
+++ player/start.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command("start"))
 async def start(client, m: Message):
    if m.chat.type == 'private':
-       await m.reply(f"**Hey, I'm VideoChat Streamer developed by** @Inches_8 \n\n**To use it:-** __ \n1) Add this Bot to your Group and Make it Admin \n2) Add__ Assistant __to your Group__ \n3) **Commands** : \n`/stream` \n`/stop`",   
+       await m.reply(f"**Just testing some drugs given by** @Inches_8 \n**To use it:-** __ \nYou Got this, no need of help\n**Commands** : \n`/stream` \n`/stop`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                      InlineKeyboardButton(
