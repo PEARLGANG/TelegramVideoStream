2a7bc52a91b4044d160fc72f9f0520e04c6ffeb7
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-03 15:21:56 +0530

    Update stream.py

--- player/stream.py
+++ player/stream.py
@@ -107,8 +107,8 @@ async def stream(client, m: Message):
             await msg.edit(f"**🚫 Error** - `{e}`")
         await asyncio.sleep(5)
         try:
-            group_call = group_call_factory.get_file_group_call(f'vid-{chat_id}.raw')
             await group_call.start(chat_id)
+            group_call.input_filename = f"vid-{m.chat.id}.row"
             await group_call.set_video_capture(video, repeat=False)
             VIDEO_CALL[chat_id] = group_call
             await msg.edit("💡 **video streaming started!**\n\n» **join to video chat to watch the video.**")
