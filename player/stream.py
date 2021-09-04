6a51e7d23f53b4177ff4a80cbc2c700a7a5dbd1b
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-04 10:58:59 +0530

    Update stream.py

--- player/stream.py
+++ player/stream.py
@@ -8,9 +8,30 @@ from config import API_ID, API_HASH, SESSION_NAME, STREAM_URL
 import schedule 
 import time 
 from youtube_dl import YoutubeDL
-
+process = None
 app = Client(SESSION_NAME, API_ID, API_HASH)
 group_call = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM).get_group_call()
+
+def raw_converter(source, output):
+    return subprocess.Popen(
+        [
+            "ffmpeg",
+            "-y",
+            "-i",
+            source,
+            "-f",
+            "rawvideo",
+            "-bsf",
+            "h264_mp4toannexb",
+            "-vcodec",
+            "copy",
+            output,
+        ],
+        stdin=None,
+        stdout=None,
+        stderr=None,
+        cwd=None,
+    )
 
 
 ydl_opts = {
@@ -22,6 +43,7 @@ links=[]
 
 @Client.on_message(filters.command("stream"))
 async def stream(client, m: Message):
+        global process
         msg = await m.reply("`Firing The Stream!`")
         try:
             meta = ydl.extract_info(STREAM_URL, download=False)
@@ -30,8 +52,10 @@ async def stream(client, m: Message):
                 links.append(f['url'])
                 finalurl=links[-1]
             print(finalurl)
+            file = f"stream(m.chat.id).mp4"
+            process = raw_converter(STREAM_URL, file)
             await group_call.join(m.chat.id)
-            await group_call.start_video(finalurl)
+            await group_call.start_video(file)
             await msg.edit("**Streaming!**")
         except Exception as e:
             await msg.edit(f"**🚫 Error** - `{e}`")
