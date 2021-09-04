f6242f6ee439f3237d1904054807547905e5960a
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-04 11:37:17 +0530

    Update stream.py

--- player/stream.py
+++ player/stream.py
@@ -19,9 +19,30 @@ ydl_opts = {
 ydl = YoutubeDL(ydl_opts)
 links=[]
 
+def raw_converter(source, output):
+    return subprocess.Popen(
+        [
+            "ffmpeg",
+            "-y",
+            "-i",
+            source,
+            "-f",
+            "rawvideo",
+            "-bsf",
+            "h264_mp4toannexb",
+            "-vcodec",
+            "copy",
+            output,
+        ],
+        stdin=None,
+        stdout=None,
+        stderr=None,
+        cwd=None,
+    )
+
 @Client.on_message(filters.command("stream"))
 async def stream(client, m: Message):
-        #global process
+        global process
         msg = await m.reply("`Firing The Stream!`")
         try:
             meta = ydl.extract_info(STREAM_URL, download=False)
@@ -30,11 +51,11 @@ async def stream(client, m: Message):
                 links.append(f['url'])
                 finalurl=links[-1]
             print(finalurl)
-            #file = f"outut.mp4"
-            #process = raw_converter(STREAM_URL, file)
+            file = f"outut.mp4"
+            process = raw_converter(finalurl, file)
             await asyncio.sleep(5) 
             await group_call.join(m.chat.id)
-            await group_call.start_video(finalurl)
+            await group_call.start_video(file)
             await msg.edit("**Streaming!**")
         except Exception as e:
             await msg.edit(f"**🚫 Error** - `{e}`")
