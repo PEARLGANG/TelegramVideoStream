bb5618f5f8fb18a1fe132bc16f62dfa8b953fe07
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-04 21:14:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ bot = Client(
     bot_token=BOT_TOKEN,
     plugins=dict(root="player"),
 )
-
-bot.start()
+Bot = Client(
+        "bot",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        workers=2
+    )
+Bot.start()
 app.start()
 idle()
