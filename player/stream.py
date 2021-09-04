e5f884d90ad06bdf51a6581257cb7b568dc43697
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-04 22:06:42 +0530

    Update stream.py

--- player/stream.py
+++ player/stream.py
@@ -42,22 +42,47 @@ def mp4_converter(source, output):
 @Client.on_message(filters.command("stream"))
 async def stream(client, m: Message):
         #global process
-        msg = await m.reply("`Firing The Stream!`")
-        try:
-            #meta = ydl.extract_info(STREAM_URL, download=False)
-            #formats = meta.get('formats', [meta])
-            #for f in formats:
-                #links.append(f['url'])
-                #finalurl=links[-1]
+        media = m.reply_to_message
+        if not media and not ' ' in m.text:
+            await m.reply("Neither YT Link Nor Media? Kek=fuck.off()!")
+
+        elif ' ' in m.text:
+            msg = await m.reply("`Firing The Stream!`")
+            text = m.text.split(' ', 1)
+            url = text[1]
+            meta = ydl.extract_info(url, download=False)
+            formats = meta.get('formats', [meta])
+            for f in formats:
+                links.append(f['url'])
+                finalurl=links[-1]
             #file = f"dr.mkv"
             #process = mp4_converter(finalurl, file)
             #await asyncio.sleep(5) 
             await group_call.join(m.chat.id)
-            await group_call.start_video(STREAM_URL)
-            await msg.edit("**Streaming!**")         
-        except Exception as e:
+            await group_call.start_video(finalurl)
+            await msg.edit("**Streaming!**")  
+        
+        elif media.video or media.document:
+            msg = await m.reply_text("`Trying to Stream the File...`")    
+            video = await client.download_media(media)
+            await group_call.start(m.chat.id)
+            await group_call.start_video(video)
+            await msg.edit("**Streaming!**")  
+
+        else Exception as e:
             await msg.edit(f"**🚫 Error** - `{e}`")
 
+ @Client.on_message(filters.command("live"))
+async def live(client, m: Message):
+        msg = await m.reply("`Firing The Stream!`")
+        try:
+            await group_call.join(m.chat.id)
+            await group_call.start_video(STREAM_URL)
+            await msg.edit("**Stream Is Live!**")         
+        except Exception as e:
+            await msg.edit(f"**🚫 Error** - `{e}`")           
+            
+            
 @Client.on_message(filters.command("stop"))
 async def stopvideo(client, m: Message):
     global process
@@ -69,3 +94,11 @@ async def stopvideo(client, m: Message):
     except Exception as e:
         await m.reply(f"**🚫 Error** - `{e}`")
 
+@Client.on_message(filters.command("lstop"))
+async def stoplive(client, m: Message):
+    try:
+        await group_call.stop()
+        caching.clear_cache()
+        await m.reply("**K Live Stopped!**")
+    except Exception as e:
+        await m.reply(f"**🚫 Error** - `{e}`")
