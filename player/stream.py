1ead4f1548ce1911172cc81ec837f30f0a9555a4
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-04 10:01:35 +0530

    Update stream.py

--- player/stream.py
+++ player/stream.py
@@ -7,17 +7,32 @@ from pyrogram.types import Message
 from config import API_ID, API_HASH, SESSION_NAME, STREAM_URL
 import schedule 
 import time 
+from youtube_dl import YoutubeDL
 
 app = Client(SESSION_NAME, API_ID, API_HASH)
 group_call = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM).get_group_call()
 
 
+ydl_opts = {
+    "geo-bypass": True,
+    "nocheckcertificate": True
+    }
+ydl = YoutubeDL(ydl_opts)
+links=[]
+
 @Client.on_message(filters.command("stream"))
 async def stream(client, m: Message):
         msg = await m.reply("`Firing The Stream!`")
         try:
+            link=m.reply_to_message.text    
+            meta = ydl.extract_info(link, download=False)
+            formats = meta.get('formats', [meta])
+            for f in formats:
+                links.append(f['url'])
+                finalurl=links[-1]
+            print(finalurl)
             await group_call.join(m.chat.id)
-            await group_call.start_video(STREAM_URL)
+            await group_call.start_video(finalurl)
             await msg.edit("**Streaming!**")
         except Exception as e:
             await msg.edit(f"**🚫 Error** - `{e}`")
