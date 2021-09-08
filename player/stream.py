79aa1d028357562874445dc48465e13f63078f13
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-09 01:02:30 +0530

    Update stream.py

--- player/stream.py
+++ player/stream.py
@@ -14,21 +14,21 @@ from youtube_dl import YoutubeDL
 process = None
 app = Client(SESSION_NAME, API_ID, API_HASH)
 group_call = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM).get_group_call()
-video = {}
+DATA = {}
 
 @Client.on_message(filters.command("stream"))
 async def stream(client, m: Message):
     try:
+        msg = await m.reply("`Firing The Stream!`")
         media = m.reply_to_message
-        gc = video.get(m.chat.id)
+        gc = DATA.get(m.chat.id)
         if gc is None:
-            video[m.chat.id] = group_call
+            DATA[m.chat.id] = group_call
         
         if not media and not ' ' in m.text:
-            await m.reply("Neither YT Link Nor Media? Kek=fuck.off()!")
+            await msg.edit("Neither YT Link Nor Media? Kek=fuck.off()!")
 
         elif ' ' in m.text:
-            msg = await m.reply("`Firing The Stream!`")
             text = m.text.split(' ', 1)
             url = text[1]
             results = YoutubeSearch(url, max_results=1).to_dict()
@@ -52,9 +52,9 @@ async def stream(client, m: Message):
 
 @Client.on_message(filters.command("live"))
 async def live(client, m: Message):
-        gc = video.get(m.chat.id)
+        gc = DATA.get(m.chat.id)
         if gc is None:
-            video[m.chat.id] = group_call
+            DATA[m.chat.id] = group_call
         msg = await m.reply("`Firing The Stream!`")
         try:
             await group_call.join(m.chat.id)
@@ -67,9 +67,9 @@ async def live(client, m: Message):
 @Client.on_message(filters.command("stop"))
 async def stopvideo(client, m: Message):
     try:
-        gc = video.get(m.chat.id)
+        gc = DATA.get(m.chat.id)
         if gc:
-            gc[m.chat.id].pop()
+            DATA[m.chat.id].pop()
             await group_call.stop()
             caching.clear_cache()
             await m.reply("**K Stopped!**")
@@ -79,7 +79,7 @@ async def stopvideo(client, m: Message):
 @Client.on_message(filters.command("lstop"))
 async def stoplive(client, m: Message):
     try:
-        gc = video.get(m.chat.id)
+        gc = DATA.get(m.chat.id)
         if gc:
             await group_call.stop()
             caching.clear_cache()
