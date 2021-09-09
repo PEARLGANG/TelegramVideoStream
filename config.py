b58c3b640957882e28ff7ade3f2febd72e90318d
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-09 10:35:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+instances = dict()
+queues = Queue()
+to_delete = []
+
 API_ID = int(os.getenv("API_ID", "6"))
 API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
